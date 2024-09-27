6bbe39e9813c20a97ff33bb7f4f097f114ccd92d
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2024-09-27 12:16:34 -0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+// Railway asigna dinámicamente un puerto, usa process.env.PORT
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`API running on http://localhost:${port}`);
+    console.log(`API running on port ${port}`);
 });
